48e2b4ff0de3ea6e71d1d7f53634dc0ba2a69e96
butteria <1340075229@qq.com>
2023-11-15 17:40:39 +0800

    update config.def.h

--- config.def.h
+++ config.def.h
@@ -51,9 +51,9 @@ static const unsigned int alphas[][3]      = {
 */
 static const char *const autostart[] = {
     "picom", NULL,
-    "feh", "--bg-fill", "/home/butteria/Pictures/wallpapers/background.png", NULL,
+    "feh", "--bg-fill", "/home/butteria/Pictures/wallpapers/dwm_bg.png", NULL,
     "fcitx5", NULL,
-    "xset", "r", "rate", "200", "40", NULL,
+    "xset r rate 200 40", NULL,
     // "motrix", "&", NULL,
     "flameshot", NULL,
 	NULL /* terminate */
@@ -81,9 +81,10 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "mpv",     NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "MATLAB",  NULL,     NULL,           1<<7,      0,          0,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
