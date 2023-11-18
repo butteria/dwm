a8c057d31f97bb030d410ec301519a442e95dcb8
butteria <1340075229@qq.com>
2023-11-18 16:02:23 +0800

    add restartsig

--- dwm.c
+++ dwm.c
@@ -260,6 +260,8 @@ static void showall(const Arg *arg);
 static void showwin(Client *c);
 static void showhide(Client *c);
 static void sigchld(int unused);
+static void sighup(int unused);
+static void sigterm(int unused);
 static void spawn(const Arg *arg);
 static void stairs(Monitor *m);
 static Monitor *systraytomon(Monitor *m);
@@ -336,6 +338,7 @@ static void (*handler[LASTEvent]) (XEvent *) = {
     [UnmapNotify] = unmapnotify
 };
 static Atom wmatom[WMLast], netatom[NetLast], xatom[XLast];
+static int restart = 0;
 static int running = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme;
@@ -1801,7 +1804,7 @@ quit(const Arg *arg)
                 if (c && HIDDEN(c)) showwin(c);
         }
     }
-
+	if(arg->i) restart = 1;
     running = 0;
 }
 
@@ -2162,6 +2165,9 @@ setup(void)
     /* clean up any zombies immediately */
     sigchld(0);
 
+	signal(SIGHUP, sighup);
+	signal(SIGTERM, sigterm);
+
     /* init screen */
     screen = DefaultScreen(dpy);
     sw = DisplayWidth(dpy, screen);
@@ -2321,6 +2327,20 @@ sigchld(int unused)
         }
 
     }
+}
+
+void
+sighup(int unused)
+{
+	Arg a = {.i = 1};
+	quit(&a);
+}
+
+void
+sigterm(int unused)
+{
+	Arg a = {.i = 0};
+	quit(&a);
 }
 
 void
@@ -3346,6 +3366,7 @@ main(int argc, char *argv[])
 #endif /* __OpenBSD__ */
     scan();
     run();
+	if(restart) execvp(argv[0], argv);
     cleanup();
     XCloseDisplay(dpy);
     return EXIT_SUCCESS;
